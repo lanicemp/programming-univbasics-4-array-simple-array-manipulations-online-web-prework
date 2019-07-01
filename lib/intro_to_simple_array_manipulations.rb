bdf7ae4d4ee8d1ce4cf0c9574b3cc2cd96f0b513
LaNice Powell <lanicemp@aol.com>
2019-07-01 03:13:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,4 @@ def using_push(array, string)
     array.push(string)
 end
   
-  def bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    new_neighborhood = "Staten Island "
-    bouroughs_in_nyc.unshift(new_neightborhood)
-  end 
+  
